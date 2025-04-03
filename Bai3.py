1ae759d091a1ad9019a365ae602e3055aa3eea35
Huy-Nguyen-Chualambo <127372269+Huy-Nguyen-Chualambo@users.noreply.github.com>
2025-04-03 16:32:28 +0700

    Update Bai3.py

--- Bai3.py
+++ Bai3.py
@@ -7,13 +7,10 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]  # Hoán đổi hai phần tử
                 swapped = True
         if not swapped:
-            break  # Thoát vòng lặp sớm nếu không có hoán đổi
+            break 
 
-# Nhập mảng từ người dùng
 arr = list(map(int, input("Nhập các số nguyên cách nhau bởi dấu cách: ").split()))
 
-# Sắp xếp mảng bằng Bubble Sort
 bubble_sort(arr)
 
-# In kết quả
 print("Mảng sau khi sắp xếp:", arr)
